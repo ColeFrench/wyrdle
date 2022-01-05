53f7e5c17867f7cdb4703f630f8305c7e0b0a7c8
Cole French <16979554+ColeFrench@users.noreply.github.com>
2022-01-05 17:34:22 -0500

    Merge inverses

--- src/wordle/frontend/hints.py
+++ src/wordle/frontend/hints.py
@@ -6,9 +6,17 @@ def _merge_correct(current: Hints.Correct, new: Hints.Correct):
     for letter, indices in new.items():
         current.setdefault(letter, set()).update(indices)
 
+def _merge_inverse_correct(current: Hints.InverseCorrect, new: Hints.InverseCorrect):
+    for index, letter in new.items():
+        current[index] = letter
+
 def _merge_misplaced(current: Hints.Misplaced, new: Hints.Misplaced):
     for letter, indices in new.items():
         current.setdefault(letter, set()).update(indices)
+
+def _merge_inverse_misplaced(current: Hints.InverseMisplaced, new: Hints.InverseMisplaced):
+    for index, letters in new.items():
+        current.setdefault(index, set()).update(letters)
 
 def _merge_absent(current: Hints.Absent, new: Hints.Absent):
     current |= new
@@ -16,5 +24,9 @@ def _merge_absent(current: Hints.Absent, new: Hints.Absent):
 def merge(current: Hints, new: Hints):
     """Merge newly received hints with the current ones."""
     _merge_correct(current.correct, new.correct)
+    _merge_inverse_correct(current.inverse_correct, new.inverse_correct)
+
     _merge_misplaced(current.misplaced, new.misplaced)
+    _merge_inverse_misplaced(current.inverse_misplaced, new.inverse_misplaced)
+
     _merge_absent(current.absent, new.absent)
