7c44a4e5a17e58bd547be75e8e6aa609a77734c3
Cole French <16979554+ColeFrench@users.noreply.github.com>
2022-01-05 12:42:14 -0500

    Scan for correct letters first
    This prevents guessed letters from consuming a match as "misplaced"
    with an upcoming letter whose guess is really "correct".

--- src/wordle/backend/hints.py
+++ src/wordle/backend/hints.py
@@ -7,24 +7,42 @@ def hint(answer: str, guess: str) -> Hints:
     hint = Hints({}, {}, set())
     working_answer = answer[:]
 
-    for guess_index, letter in enumerate(guess):
-        try:
-            answer_index = working_answer.index(letter)
-        except ValueError:  # "Absent"
-            hint.absent.add(letter)
+    # First, scan for correct letters
+    for guess_letter_index, guess_letter in enumerate(guess):
+        if working_answer[guess_letter_index] == guess_letter:
+            # This letter is correct; register its location
+            hint.correct.setdefault(guess_letter, set()).add(guess_letter_index)
+
+            # Let's remove this letter from our working answer, so we don't
+            # match its specific occurrence again when scanning for misplaced
+            # and absent letters.
+            #
+            # We should replace it with an unmatchable character rather than
+            # remove it outright, since we want to preserve the locations of
+            # the remaining letters.
+            working_answer = working_answer[:guess_letter_index] \
+                           + ' ' \
+                           + working_answer[guess_letter_index + 1:]
+
+    # Now, scan for misplaced and absent letters
+    for guess_letter_index, guess_letter in enumerate(guess):
+        if guess_letter_index in hint.correct.get(guess_letter, set()):
+            # Skip correct letters
             continue
 
-        # The letter is present. Let's remove it from the answer so we don't
-        # match this specific occurrence again. We should replace it with an
-        # unmatchable character rather than remove it outright, since we want
-        # to preserve the locations of other letters.
-        working_answer = working_answer[:answer_index] \
-                       + ' ' \
-                       + working_answer[answer_index + 1:]
+        try:
+            answer_letter_index = working_answer.index(guess_letter)
+        except ValueError:
+            # This letter is absent; register it
+            hint.absent.add(guess_letter)
+            continue
 
-        if guess_index == answer_index:  # "Correct"
-            hint.correct[letter] = guess_index
-        else:  # "Misplaced"
-            hint.misplaced[letter] = guess_index
+        # This letter is misplaced; register its location
+        hint.misplaced.setdefault(guess_letter, set()).add(guess_letter_index)
+
+        # Again, prevent further matches
+        working_answer = working_answer[:answer_letter_index] \
+                       + ' ' \
+                       + working_answer[answer_letter_index + 1:]
 
     return hint
