ec545afd7e808808d0c039422e880cdd8a0124a3
Cole French <16979554+ColeFrench@users.noreply.github.com>
2022-01-11 16:57:02 -0500

    Construct inverse mappings based on forward mappings

--- src/wyrdle/common/hints.py
+++ src/wyrdle/common/hints.py
@@ -8,9 +8,19 @@ class Hints:
     """A summary of the information the player receives each round."""
 
     correct: Dict[str, Set[int]] = field(default_factory=dict)
-    inverse_correct: Dict[int, str] = field(default_factory=dict)
+    inverse_correct: Dict[int, str] = field(default_factory=dict, init=False)
 
     misplaced: Dict[str, Set[int]] = field(default_factory=dict)
-    inverse_misplaced: Dict[int, Set[str]] = field(default_factory=dict)
+    inverse_misplaced: Dict[int, Set[str]] = field(default_factory=dict, init=False)
 
     absent: Set[str] = field(default_factory=set)
+
+    def __post_init__(self):
+        """Initialize inverse mappings."""
+        for letter, indices in self.correct.items():
+            for index in indices:
+                self.inverse_correct[index] = letter
+
+        for letter, indices in self.misplaced.items():
+            for index in indices:
+                self.inverse_misplaced.setdefault(index, set()).add(letter)
