1c21503c976eadc8f37b636eae4aa5553e17f222
Cole French <16979554+ColeFrench@users.noreply.github.com>
2022-01-12 21:32:33 -0500

    Begin developing CLI

---
+++ src/wyrdle/frontend/interface.py
@@ -0,0 +1,8 @@
+"""User interface."""
+
+import argparse
+
+def cli():
+    """Command-line interface."""
+    parser = argparse.ArgumentParser(description="Interact with Wordle.")
+    parser.parse_args()
