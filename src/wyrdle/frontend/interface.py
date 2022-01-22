ece302e7c77fbce18e8f68ced7411826db47cbb7
Cole French <16979554+ColeFrench@users.noreply.github.com>
2022-01-22 10:27:03 -0500

    Allow custom answer

--- src/wyrdle/frontend/interface.py
+++ src/wyrdle/frontend/interface.py
@@ -6,14 +6,21 @@ from tap import Tap
 from .game import Game
 
 class _ArgumentParser(Tap):
+    word: str = 'today\'s'  # which word to make the answer
     bot: bool = False  # have the bot play instead
 
     def configure(self):
+        self.add_argument('-w', '--word')
         self.add_argument('-b', '--bot')
 
 def cli():
     """Command-line interface."""
     args = _ArgumentParser(description="Interact with Wordle.").parse_args()
+    game_kwargs = {
+        'bot': args.bot,
+    }
+    if args.word != 'today\'s':
+        game_kwargs['answer'] = args.word
 
     colorama.init()
-    Game(bot=args.bot).play()
+    Game(**game_kwargs).play()
